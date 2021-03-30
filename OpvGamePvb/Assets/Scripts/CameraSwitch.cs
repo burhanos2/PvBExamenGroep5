e82e8691f2f2c6a4460a12399858e3600cc58d58
burhanos2 <burhantopaloglu60@gmail.com>
2021-03-30 18:14:59 +0200

    Added functionality to swap between two camera's

---
+++ OpvGamePvb/Assets/Scripts/CameraSwitch.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class CameraSwitch : MonoBehaviour
+{
+    [SerializeField]private Camera cam1, cam2;
+    private void Start()
+    {
+        Control.OnSwitchKey += SwitchCameraDepths;
+    }
+
+    private void SwitchCameraDepths() // this temp method merely illustrates how to swap cams
+    {
+        var cam1Temp = cam1.depth;
+        cam1.depth = cam2.depth;
+        cam2.depth = cam1Temp;
+    }
+}
---
+++ OpvGamePvb/Assets/Scripts/Control.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Control : MonoBehaviour
+{
+    [SerializeField] private KeyCode switchingKey = KeyCode.Space;
+    public static Action OnSwitchKey;
+    private void Update()
+    {
+        if (Input.GetKeyDown(switchingKey))
+        {
+            OnSwitchKey?.Invoke();
+        }
+    }
+}
