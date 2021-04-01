5d0ef8eb02bd251689f233c4958f6ada70145fe0
burhanos2 <burhantopaloglu60@gmail.com>
2021-04-01 14:47:00 +0200

    added blinking animation and removed additional camera's
    instead of having multiple camera's, the camera will teleport between set points now

---
+++ OpvGamePvb/Assets/Scripts/BlinkingAnimation/AnimatorValueSetter.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+//this class sets the isBlind bool on the blink animator and resets the play trigger
+namespace BlinkingAnimation
+{
+    public class AnimatorValueSetter : MonoBehaviour
+    {
+        private Animator m_Animator;
+        private static readonly int IsBlind = Animator.StringToHash("isBlind");
+        private static readonly int PlayBlink = Animator.StringToHash("PlayBlink");
+
+        private void Start()
+        {
+            m_Animator = GameObject.Find("Camera_Eyelids").GetComponent<Animator>();
+        }
+
+        private void SetBlind(int value)
+        {
+            switch (value)
+            {
+                case 0:
+                    m_Animator.SetBool(IsBlind, false);
+                    break;
+                case 1: m_Animator.SetBool(IsBlind, true);
+                    break;
+            }
+        }
+
+        private void ResetTrigger()
+        {
+            m_Animator.ResetTrigger(PlayBlink);
+        }
+    }
+}
---
+++ OpvGamePvb/Assets/Scripts/BlinkingAnimation/SimpleAnimationTrigger.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+//this is to show how to trigger said animation
+public class SimpleAnimationTrigger : MonoBehaviour
+{
+    private Animator cameraEyelidBlink;
+    void Start()
+    {
+        cameraEyelidBlink = GameObject.Find("Camera_Eyelids").GetComponent<Animator>();
+        
+        Control.OnSwitchKey += DoBlinkingAnim;
+    }
+
+    private void DoBlinkingAnim()
+    {
+        cameraEyelidBlink.SetTrigger("PlayBlink");
+    }
+}
