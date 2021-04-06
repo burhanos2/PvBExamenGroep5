15a97f7284e20dd65d13f0629171d1b5cc8dba77
burhanos2 <burhantopaloglu60@gmail.com>
2021-04-06 20:21:59 +0200

    added WaveEditor file
    this file houses the variables for the waves and is made to be used with the unity editor

---
+++ OpvGamePvb/Assets/Scripts/WaveSystem/WavesEditor.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WaveSystem
+{
+    [ExecuteInEditMode]
+    public class WavesEditor : MonoBehaviour
+    {
+        [SerializeField, Range(1, 20)] 
+        private int _waveAmount = 1;
+        [SerializeField, Tooltip("Amount of Enemies that should spawn in specified wave.")] 
+        private List<int> _enemiesPerWave = new List<int>();
+        
+        public int GetEnemyAmountThisWave => _enemiesPerWave[_currentWave - 1]; // getter for current wave
+        public List<Transform> _enemySpawners = new List<Transform>();
+        public int _currentWave = 1;
+        public Action<int> OnNextWave; // int returns next wave number
+        
+        #if (UNITY_EDITOR)
+            private void Update()
+            {
+                if (_waveAmount == _enemiesPerWave.Count) return; // if it is the same, dont bother going through the updating process
+                UpdateList();
+            }
+        #endif
+        
+        private void UpdateList()
+        {
+            if (_waveAmount > _enemiesPerWave.Count)
+            {
+                FillWaves((_waveAmount - _enemiesPerWave.Count));
+            }
+            else if (_waveAmount < _enemiesPerWave.Count)
+            {
+                _enemiesPerWave.RemoveRange(_waveAmount, (_enemiesPerWave.Count - _waveAmount));
+            }
+        }
+        
+        private void FillWaves(int amount)
+        {
+            var v = amount;
+            for (int i = 0; i < v; i++)
+            {
+                _enemiesPerWave.Add(1);
+            }
+        }
+    }
+}
