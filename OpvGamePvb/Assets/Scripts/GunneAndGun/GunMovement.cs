1df507cb0e43de32536223008e7c76fbcde3bb02
DragstylerNL <dragstylernl@gmail.com>
2021-04-06 10:12:19 +0200

    moving of things before combining

--- OpvGamePvb/Assets/Scripts/GunneAndGun/GunMovement.cs
+++ OpvGamePvb/Assets/Scripts/GunneAndGun/GunMovement.cs
@@ -1,12 +1,10 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class GunMovement : MonoBehaviour
 {
     [SerializeField] [Range(1, 20)]
-    private int gunRotateSpeed = 4;
+    private int _gunRotateSpeed = 4;
     [SerializeField]
     public GameObject gunObject;
     [SerializeField]
@@ -14,13 +12,6 @@ public class GunMovement : MonoBehaviour
 
     public Action Shoot;
     
-    // Start is called before the first frame update
-    private void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     private void Update()
     {
         TurnHorizontal();
@@ -37,11 +28,11 @@ public class GunMovement : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.Mouse1))
         {
-            gunObject.transform.Rotate(0 ,yAngle: + gunRotateSpeed*Time.deltaTime ,0);
+            gunObject.transform.Rotate(0 ,yAngle: + _gunRotateSpeed*Time.deltaTime ,0);
         } 
         else if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.Mouse0))
         {
-            gunObject.transform.Rotate(0 ,yAngle: - gunRotateSpeed*Time.deltaTime ,0);
+            gunObject.transform.Rotate(0 ,yAngle: - _gunRotateSpeed*Time.deltaTime ,0);
         }
     }
 
@@ -49,11 +40,11 @@ public class GunMovement : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.UpArrow) || Input.GetAxis("Mouse ScrollWheel") > 0f)
         {
-            barrelObject.transform.Rotate(+ gunRotateSpeed*Time.deltaTime ,yAngle:0  ,0);
+            barrelObject.transform.Rotate(+ _gunRotateSpeed*Time.deltaTime ,yAngle:0  ,0);
         } 
         else if (Input.GetKey(KeyCode.DownArrow) || Input.GetAxis("Mouse ScrollWheel") < 0f)
         {
-            barrelObject.transform.Rotate( - gunRotateSpeed*Time.deltaTime  ,yAngle:0,0);
+            barrelObject.transform.Rotate( - _gunRotateSpeed*Time.deltaTime  ,yAngle:0,0);
         }
     }
 }
---
+++ OpvGamePvb/Assets/Scripts/GunneAndGun/PlayerBullet.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class PlayerBullet : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody _bulletRb;
+    [SerializeField, Range(2, 50)]
+    private float _bulletSpeed = 2;
+    
+    private GameObject _aim;
+    private Transform _target;
+    private GameObject _landingPlace;
+    
+    
+    private void Start()
+    {
+        _landingPlace = GameObject.Find("LandingPlace");
+        _aim = GameObject.FindWithTag("BarrelEnd");
+        _target = GameObject.FindWithTag("BarrelBegin").transform;
+        Vector3 direction = (_aim.transform.position - _target.transform.position).normalized;
+        _bulletRb.velocity = (direction * _bulletSpeed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("EnemyShip"))
+        {
+            DeleteBullet();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("PlayArea"))
+        {
+            DeleteBullet();
+        }
+    }
+
+    private void DeleteBullet()
+    {
+        _landingPlace.transform.position = gameObject.transform.position;
+        Destroy(this.gameObject);
+    }
+}
