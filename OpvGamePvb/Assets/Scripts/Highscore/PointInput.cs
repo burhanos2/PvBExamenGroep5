cf3c30591de9181122fbc51040ea8a453026d392
PieterWinderlichSchool <pieterwinderlich@outlook.com>
2021-04-08 12:56:28 +0200

    Added Point middleman
    Added  Point Input that serves as a middleman between the point counter and the enemy's. This script has functionality to add modifiers and point addition

---
+++ OpvGamePvb/Assets/Scripts/Highscore/PointInput.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using UnityEngine;
+
+public class PointInput : MonoBehaviour
+{
+    private int _PointMultiplier;
+    [SerializeField]
+    private ScoreKeeping scoreKeeper;
+    // Start is called before the first frame update
+
+
+    // Update is called once per frame
+
+
+    public void ObtainPoints(int pointsToAdd)
+    {
+      scoreKeeper.UpdateScore(pointsToAdd *=_PointMultiplier);  
+    }
+
+    public void AddMultuplier(int newMultiplier)
+    {
+        if (_PointMultiplier > 20)
+        {
+            _PointMultiplier += newMultiplier;
+        }
+       
+    }
+
+    public void ResetMultiplier()
+    {
+        _PointMultiplier = 0;
+    }
+}
