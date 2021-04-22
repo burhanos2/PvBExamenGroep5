962f6696235acc0ff389a86444424ce3866200f4
burhanos2 <burhantopaloglu60@gmail.com>
2021-04-22 10:14:03 +0200

    edited characterstates, can now disable and enable UI

--- OpvGamePvb/Assets/Scripts/Characters/Cannoneer.cs
+++ OpvGamePvb/Assets/Scripts/Characters/Cannoneer.cs
@@ -3,10 +3,12 @@
     public override void Enter()
     {
         // update controls here and start processes
+        SetArrayOfGO(_objectsToEnable, true);
     }
 
     public override void Leave()
     {
         //halt controls here and halt processes
+        SetArrayOfGO(_objectsToDisable, false);
     }
 }
