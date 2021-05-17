6a37e6ea39d6a9845e3baf5bd14b3be4069c95ea
burhanos2 <burhantopaloglu60@gmail.com>
2021-05-17 22:18:55 +0200

    added GetBoundsOfNearestArea method to EnemyPlayAreaManager

--- OpvGamePvb/Assets/Scripts/Enemy/PlayArea/EnemyPlayAreaManager.cs
+++ OpvGamePvb/Assets/Scripts/Enemy/PlayArea/EnemyPlayAreaManager.cs
@@ -29,13 +29,30 @@ public class EnemyPlayAreaManager : MonoBehaviour
     }
     
     /// <summary>
-    /// (WIP) returns a vector 4 of bounds (x MIN, x MAX, z MIN, z MAX)
+    /// returns a vector 4 of bounds (x MIN, x MAX, z MIN, z MAX)
     /// </summary>
     /// <param name="closestToPoint">get the area closest to point</param>
     /// <returns>returns a vector 4 of bounds (x MIN, x MAX, z MIN, z MAX)</returns>
     public Vector4 GetBoundsOfNearestArea(Vector3 closestToPoint)
     {
+        GameObject closestObj = null;
+        var minDist = Mathf.Infinity;
+        var searchFor = closestToPoint;
+        foreach (var obj in GetPlayAreaObjects)
+        {
+            var dist = Vector3.Distance(obj.transform.position, searchFor);
+            if (!(dist < minDist)) continue;
+            closestObj = obj;
+            minDist = dist;
+        }
 
-        return Vector4.zero;
+        if (closestObj == null)
+        {
+            Debug.LogError("closestObject == null");
+            return Vector4.zero;
+        }
+        var closestBounds = closestObj.GetComponent<Renderer>().bounds;
+        var fin = new Vector4(closestBounds.min.x, closestBounds.max.x, closestBounds.min.z, closestBounds.max.z);
+        return fin;
     }
 }
