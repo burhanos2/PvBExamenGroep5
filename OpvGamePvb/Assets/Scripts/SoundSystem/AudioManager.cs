999e65adf38257289f4aa2163ee2389c72f6c27e
burhanos2 <burhantopaloglu60@gmail.com>
2021-05-26 13:24:50 +0200

    AudioManager written, now needs to be implemented. AmbienceManager created but not written.

---
+++ OpvGamePvb/Assets/Scripts/SoundSystem/AudioManager.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace SoundSystem
+{
+   public enum SfxTypes 
+   {
+      CannonShoot = 0,
+      Birds = 1,
+   }
+
+   public enum AmbienceTypes
+   {
+      
+   }
+
+   public class AudioManager : MonoBehaviour
+   {
+      public static AudioManager Instance;
+      private void Awake()
+      {
+         Instance = this;
+      }
+      
+      [SerializeField] private AudioSource _ambience;
+      [SerializeField] private AudioSource _sfx;
+      
+      // it is very important that these arrays align with the enumerators
+      [SerializeField] private SoundClipVariantGroup[] _sfxVariantGroups; 
+      [SerializeField] private SoundClipVariantGroup[] _ambienceVariantGroups;
+
+      private int _musicIndex;
+      private bool _musicIsPlaying;
+      private bool _ambienceIsPlaying;
+
+      [System.Serializable]
+      private class SoundClipVariantGroup
+      {
+         [SerializeField] private string _name; //I WISH I KNEW THIS WAS POSSIBLE WHEN I DID THE CUSTOM WAVE SYSTEM... set name of cluster here
+         public AudioClip[] _sounds; //set clips here
+         public float[] _volumes; // set different levels of volume for variation here
+      }
+
+      public void PlayRandomSfxVariant(SfxTypes index)
+      {
+         var shortcutSounds = _sfxVariantGroups[(int) index]._sounds;
+         var shortcutVolume = _sfxVariantGroups[(int)index]._volumes;
+         _sfx.PlayOneShot( 
+            shortcutSounds[Random.Range(0, shortcutSounds.Length - 1)],
+            Random.Range(shortcutVolume[0], shortcutVolume[shortcutVolume.Length - 1])
+            );
+      }
+      
+      public void PlayRandomAmbienceVariant(AmbienceTypes index)
+      {
+         var shortcutSounds = _ambienceVariantGroups[(int) index]._sounds;
+         var shortcutVolume = _ambienceVariantGroups[(int)index]._volumes;
+         _ambience.PlayOneShot( 
+            shortcutSounds[Random.Range(0, shortcutSounds.Length - 1)],
+            Random.Range(shortcutVolume[0], shortcutVolume[shortcutVolume.Length - 1])
+         );
+      }
+   }
+}
