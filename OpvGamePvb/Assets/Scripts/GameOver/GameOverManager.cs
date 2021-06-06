74cbb8ac15cc3f5fa60fb7de4c0b4fecf9b1be01
burhanos2 <burhantopaloglu60@gmail.com>
2021-06-06 23:40:26 +0200

    Made the button at the end restart the game

--- OpvGamePvb/Assets/Scripts/GameOver/GameOverManager.cs
+++ OpvGamePvb/Assets/Scripts/GameOver/GameOverManager.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class GameOverManager : MonoBehaviour
@@ -74,5 +75,10 @@ public class GameOverManager : MonoBehaviour
         }
 
         yield return null;
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(0);
     }
 }
