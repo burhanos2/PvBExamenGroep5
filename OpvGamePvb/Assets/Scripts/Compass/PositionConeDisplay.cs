a386e71848eb1129793f5af27e4bfe9329957f0c
PieterWinderlichSchool <pieterwinderlich@outlook.com>
2021-06-17 11:59:04 +0200

    Added Compass
    Added a working compass with wind directions and following FoV cones

---
+++ OpvGamePvb/Assets/Scripts/Compass/PositionConeDisplay.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class PositionConeDisplay : MonoBehaviour
+{
+    [SerializeField]
+    private List<RectTransform> _cones;
+     [SerializeField]
+    private List<Transform> _rotationPoints;
+
+   private void Update()
+    {
+        for (int i = 0; i < _cones.Count; i++)
+        {
+            _cones[i].localRotation = new Quaternion(_rotationPoints[i].rotation.x,_rotationPoints[i].rotation.z,-_rotationPoints[i].rotation.y,_rotationPoints[i].rotation.w);
+            
+        }
+            
+        
+    }
+}
